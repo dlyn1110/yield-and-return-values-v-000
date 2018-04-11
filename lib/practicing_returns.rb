6f5d06e71ea8c6fe92480ea1277c0f81f2aa94c3
Dani Lyn <dlyn1110@gmail.com>
2018-04-11 14:30:08 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,9 +4,9 @@ def hello(array)
   i = 0
   while i < array.length
     binding.pry
-   yield(array[i])
+    yield(array[i])
     i += 1
   end
 end
 
-hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
